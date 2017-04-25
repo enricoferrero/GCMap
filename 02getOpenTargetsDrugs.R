3d2d11750b7d6c98168bd8be069d3f18c59a4443
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2017-04-25 12:02:06 +0100

    use API key

--- 02getOpenTargetsDrugs.R
+++ 02getOpenTargetsDrugs.R
@@ -8,10 +8,13 @@ registerDoParallel(parallel::detectCores() - 1)
 # read STOPGAP data to get EFO IDs for diseases
 stopgap <- fread("../dat/stopgap.tsv")
 
+# request token for Open Targets API
+token <- content(GET("https://www.targetvalidation.org/api/latest/public/auth/request_token", query = list(app_name = "personal", secret = "8r88SQNM2A2nayCd47rSWVTh7x5DSw47")))$token
+
 # query the Open Targets API to get all known drugs for each disease
 efo.ids <- unique(stopgap[, efo.id])
 opentargets  <- foreach(i = seq(efo.ids), .combine = rbind) %dopar% {
-    tmp <- fromJSON(content(GET(paste0("https://www.targetvalidation.org/api/latest/public/evidence/filter?disease=", efo.ids[i], "&datatype=known_drug&scorevalue_min=0.2&size=10000")), type = "text", encoding = "UTF-8"))
+    tmp <- fromJSON(content(GET("https://www.targetvalidation.org/api/latest/public/evidence/filter", query = list(disease = efo.ids[i], datatype = "known_drug", scorevalue_min = 0.2, size = 10000), add_headers(`Auth-Token` = token)), type = "text", encoding = "UTF-8"))
     if (class(tmp$data$drug) == "data.frame") {
         tmp <- unique(as.data.table(flatten(tmp$data$drug)))
         tmp[, c("efo.id", "chembl.id") := .(efo.ids[i], sub("http://identifiers.org/chembl.compound/", "", id))]
