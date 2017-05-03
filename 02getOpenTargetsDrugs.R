63c56ba89312e6312b382250dee655c3020fb9bd
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2017-05-03 18:11:28 +0100

    better use of Open Targets API

--- 02getOpenTargetsDrugs.R
+++ 02getOpenTargetsDrugs.R
@@ -8,14 +8,26 @@ registerDoParallel(parallel::detectCores() - 1)
 # read STOPGAP data to get EFO IDs for diseases
 stopgap <- fread("../dat/stopgap.tsv")
 
-# request token for Open Targets API
+## query the Open Targets API to get all known drugs for each disease
+# get token
 token <- content(GET("https://www.targetvalidation.org/api/latest/public/auth/request_token", query = list(app_name = "personal", secret = "8r88SQNM2A2nayCd47rSWVTh7x5DSw47")))$token
 
-# query the Open Targets API to get all known drugs for each disease
+# loop thorugh diseases
 efo.ids <- unique(stopgap[, efo.id])
 opentargets  <- foreach(i = seq(efo.ids), .combine = rbind) %dopar% {
-    tmp <- fromJSON(content(GET("https://www.targetvalidation.org/api/latest/public/evidence/filter", query = list(disease = efo.ids[i], datatype = "known_drug", scorevalue_min = 0.2, size = 10000), add_headers(`Auth-Token` = token)), type = "text", encoding = "UTF-8"))
-    if (class(tmp$data$drug) == "data.frame") {
+
+    # query API
+    tmp <- GET("https://www.targetvalidation.org/api/latest/public/evidence/filter", query = list(disease = efo.ids[i], datatype = "known_drug", size = 10000), add_headers(`Auth-Token` = token))
+
+    # check response and request new token if necessary
+    if (tmp$status_code == 419) {
+        token <- content(GET("https://www.targetvalidation.org/api/latest/public/auth/request_token", query = list(app_name = "personal", secret = "8r88SQNM2A2nayCd47rSWVTh7x5DSw47")))$token
+        tmp <- GET("https://www.targetvalidation.org/api/latest/public/evidence/filter", query = list(disease = efo.ids[i], datatype = "known_drug", size = 10000), add_headers(`Auth-Token` = token))
+    }
+
+    # extract and check data
+    tmp <- fromJSON(content(tmp, type = "text", encoding = "UTF-8"))
+    if (length(tmp$data) > 0) {
         tmp <- unique(as.data.table(flatten(tmp$data$drug)))
         tmp[, c("efo.id", "chembl.id") := .(efo.ids[i], sub("http://identifiers.org/chembl.compound/", "", id))]
     }
